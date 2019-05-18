bc07e79c48d07bd0cccebd7b5087b91a9025e146
Sionut0122 <sionut0122@yahoo.com>
2019-05-18 11:51:55 +0100

    Updates

---
+++ precache-manifest.d31cf563887a536e9223759cc63853f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc0351dc02ed7015b56efdec69287c23",
+    "url": "/ecom/static/media/plexus_2.dc0351dc.mp4"
+  },
+  {
+    "revision": "8348f3bb3c03f99d2df830e7fd5830ba",
+    "url": "/ecom/static/media/logo.8348f3bb.jpg"
+  },
+  {
+    "revision": "8e15b711f719536a30648efc42f055c4",
+    "url": "/ecom/static/media/bg_video_image.8e15b711.png"
+  },
+  {
+    "revision": "24b25aef5b9048a00409",
+    "url": "/ecom/static/js/runtime~main.a345f0af.js"
+  },
+  {
+    "revision": "9c53b38d89e6c61ec6dc",
+    "url": "/ecom/static/js/main.e7204e86.chunk.js"
+  },
+  {
+    "revision": "897ae4ea4aa88be75421",
+    "url": "/ecom/static/js/2.902fff6d.chunk.js"
+  },
+  {
+    "revision": "9c53b38d89e6c61ec6dc",
+    "url": "/ecom/static/css/main.873469e5.chunk.css"
+  },
+  {
+    "revision": "122362a6ab5383cb2ef9a2a0922be961",
+    "url": "/ecom/index.html"
+  }
+];
